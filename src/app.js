e0d7ab2033650ccfefdfb6ab0c5bfc7f8c41d0f9
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-04-20 02:28:38 +0300

    start

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className={'first'}>tictactoe  1</h1>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom"
+import '/src/style/style.scss'
+import App from '/src/app'
 
 ReactDOM.render(
-    <h1>Hello</h1>,
+    <App/>,
     document.getElementById('app')
 )
