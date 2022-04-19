d183118235706f538bfa7b9490d8626cf5812837
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-04-20 01:34:39 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'tictactoe',
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                "@babel/preset-react",
+                                {
+                                    plugins: ["@babel/plugin-proposal-class-properties"],
+                                },
+                            ]
+                        }
+                    }
+            },
+
+        ],
+    },
+};
