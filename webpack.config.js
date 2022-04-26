70511662dd398dba5bed41d9204df57adc1a64d4
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-04-26 23:15:39 +0300

    add koa server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'server'),
         filename: 'index_bundle.js'
     },
 
